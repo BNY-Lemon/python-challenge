9abbd68efefa30a72d5ba10d6822c60498287b48
BNY-Lemon <BNY-Lemon@users.noreply.github.com>
2020-08-12 15:50:05 -0500

    Finished pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,15 +18,33 @@ with open(election_data_filepath) as csv_file:
 	# print the csv reader object
     for row in csv_reader:
         if row[2] not in tracking_votes.keys():
-            #adding a candidate to a candidate arry if it is not there
-            #candidates.append(row[2])
+            #adding a candidate to a candidate array if it is not there
+            candidates.append(row[2])
             #initialize a candidate in the dictionnary with 0 votes 
             tracking_votes[row[2]] = 1
-            print(tracking_votes)
+            total_votes += 1
         else:
             tracking_votes[row[2]] += 1
-            #print(tracking_votes)
+            total_votes += 1
 
-# print(tracking_votes['Khan'])
-# print(tracking_votes.keys())
-# output to text file
+tracking_votes["OTooley"] = tracking_votes.pop("O'Tooley")
+winner = max(tracking_votes, key=tracking_votes.get)
+# Output
+print_string = "Election Results\n" \
+	"-------------------------\n" \
+    f"Total Votes: {total_votes}\n" \
+    "-------------------------\n" \
+    f"Khan: {(tracking_votes['Khan'] / total_votes):.3%} ({tracking_votes['Khan']})\n" \
+    f"Correy: {(tracking_votes['Correy'] / total_votes):.3%} ({tracking_votes['Correy']})\n" \
+    f"Li: {(tracking_votes['Li'] / total_votes):.3%} ({tracking_votes['Li']})\n" \
+    f"O'Tooley: {(tracking_votes['OTooley'] / total_votes):.3%} ({tracking_votes['OTooley']})\n" \
+    "-------------------------\n" \
+    f"Winner: {winner}\n" \
+    "-------------------------" 
+    
+    
+print(print_string)
+
+# Output to text file
+with open(os.path.join("Analysis", "output.txt"), "w") as file:
+	file.write(print_string)
